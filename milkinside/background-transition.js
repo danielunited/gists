f5a5c045fb58bc671f676da6d1f107156b5e8bb2
Daniel Alter <daniel.united@windowslive.com>
2023-01-10 22:17:40 +0200

    Updated Jerome's script

--- milkinside/background-transition.js
+++ milkinside/background-transition.js
@@ -1,191 +1,145 @@
 !function($) {
 
-  const positions = [
-    {
-      /** 1 */
-      web: {
-        forward: ['00:00:00', '00:00:30'],
-        now: ['00:02:02', '00:06:01'],
-        back: ['00:01:01', '00:02:01'],
-      },
-      mobile: {
-        forward: ['00:00:00', '00:00:30'],
-        now: ['00:02:12', '00:06:11'],
-        back: ['00:01:01', '00:02:11'],
-      }
-    },
-    {
-      /** 2 */
-      web: {
-        forward: ['00:06:03', '00:07:02'],
-        now: ['00:08:03', '00:12:02'],
-        back: ['00:07:03', '00:07:56'],
-      },
-      mobile: {
-        forward: ['00:06:03', '00:07:02'],
-        now: ['00:08:13', '00:12:12'],
-        back: ['00:07:03', '00:08:12'],
-      }
-    },
-    {
-      /** 3 */
-      web: {
-        forward: ['00:12:03', '00:15:08'],
-        now: ['00:18:22', '00:21:14'],
-        back: ['00:15:09', '00:18:21'],
-      },
-      mobile: {
-        forward: ['00:12:03', '00:15:08'],
-        now: ['00:18:29', '00:21:34'],
-        back: ['00:15:09', '00:18:26'],
-      }
-    },
-    {
-      /** 4 */
-      web: {
-        forward: ['00:21:15', '00:23:44'],
-        now: ['00:26:14', '00:30:01'],
-        back: ['00:23:45', '00:26:03'],
-      },
-      mobile: {
-        forward: ['00:21:34', '00:23:54'],
-        now: ['00:26:24', '00:30:01'],
-        back: ['00:23:55', '00:25:54'],
-      }
-    },
-    {
-      /** 5 */
-      web: {
-        forward: ['00:30:02', '00:31:05'],
-        now: ['00:32:50', '00:36:10'],
-        back: ['00:31:16', '00:32:19'],
-      },
-      mobile: {
-        forward: ['00:30:02', '00:31:05'],
-        now: ['00:32:50', '00:36:10'],
-        back: ['00:31:06', '00:32:09'],
-      }
-    },
-    {
-      /** 6 */
-      web: {
-        forward: ['00:36:11', '00:37:34'],
-        now: ['00:38:53', '00:42:11'],
-        back: ['00:37:35', '00:38:42'],
-      },
-      mobile: {
-        forward: ['00:36:11', '00:37:34'],
-        now: ['00:38:53', '00:42:11'],
-        back: ['00:37:35', '00:38:22'],
-      }
-    },
-    {
-      /** 7 */
-      web: {
-        forward: ['00:42:12', '00:45:26'],
-        now: ['00:48:34', '00:51:24'],
-        back: ['00:45:26', '00:48:28'],
-      },
-      mobile: {
-        forward: ['00:42:12', '00:45:26'],
-        now: ['00:48:54', '00:51:44'],
-        back: ['00:45:26', '00:48:38'],
-      }
-    },
-    {
-      /** 8 */
-      web: {
-        forward: ['00:51:15', '00:56:14'],
-        now: ['01:01:55', '01:05:08'],
-        back: ['00:56:25', '01:01:14'],
-      },
-      mobile: {
-        forward: ['00:51:45', '00:56:14'],
-        now: ['01:02:05', '01:05:08'],
-        back: ['00:56:25', '01:01:14'],
-      }
-    },
-  ];
+  const hashValue = parseInt(window.location.hash.replace('#',''));
+  const frameGap = 0.25; /** gap front and end of loop, intro and outro */
+  const positions = []; /** collective timeframe supplied at the section area of the template */
+  const videos = $("#video-hero, #video-hero-mobile");
+  $('.main .section').toArray().forEach(dom => {
+    positions.push({
+      forward: [$(dom).attr('transitionstart'), $(dom).attr('transitionend')],
+      now: [$(dom).attr('loopstart'), $(dom).attr('loopend')],
+      back: [$(dom).attr('transitionstartbackward'), $(dom).attr('transitionendbackward')],
+    })
+  });
 
-  let hashValue = parseInt(window.location.hash.replace('#',''));
-  const isMobile = $("#video-hero-mobile").is(":visible");
   let currentIndex = hashValue ? hashValue - 1 : 0;
   let deltaIndex = currentIndex;
-  let singleFrame = .16;
   let startTimeWeb = 0;
   let endTimeWeb = 0;
-  let playbackRate = 1;
+  let isSkip = false;
 
   const convertTime = (time) => {
-    const [min, sec, mil] = time.split(':')
-    return Math.max((parseInt(min) * 60) + parseInt(sec) + (parseInt(mil) / 60),.01).toFixed(3)
+    const [min, sec, frames] = time.split(':')
+    /** MINUTES:SECONDS:HUNDREDTHS */
+    return parseFloat(Math.max((parseInt(min) * 60) + parseInt(sec) + ((parseInt(frames) /100)),.01).toFixed(3))
   };
 
   const convertTimeRange = (timeRange) => {
-    return [convertTime(timeRange[0]), convertTime(timeRange[1])];
+    return [convertTime(timeRange[0]) + frameGap, convertTime(timeRange[1]) - frameGap];
   };
 
-  const playQueue = [
-    convertTimeRange(positions[currentIndex ? currentIndex - 1 : 0][isMobile ? 'mobile' : 'web'].forward),
-    convertTimeRange(positions[currentIndex ? currentIndex - 1 : 0][isMobile ? 'mobile' : 'web'].now)
+  const initializeVideo = () => {
+    videos.each(i => {
+      if (!$(videos[i]).is(":hidden")) {
+        videos[i].play()
+          .then(() => {
+            videos[i].pause();
+            console.warn('Browser with Autoplay');
+            setTimeout(() => videos[i].play(), 3000);
+          }).catch(() => {
+          console.warn('Browser without Autoplay');
+          setTimeout(() => videos[i].play(), 3000);
+        })
+      }
+    });
+  };
+
+  let playQueue = [
+    convertTimeRange(positions[currentIndex].forward),
+    convertTimeRange(positions[currentIndex].forward),
+    convertTimeRange(positions[currentIndex].now)
   ];
 
-  $("#video-hero, #video-hero-mobile").bind("timeupdate", function () {
-    if (this.currentTime > endTimeWeb - singleFrame) {
+  videos.bind("timeupdate", function () {
+    if (this.currentTime > endTimeWeb) {
+      /** remove top playable */
       if (playQueue.length > 1) {
         playQueue.shift();
-      } else {
-        this.playbackRate = 1;
-        playbackRate = 1;
       }
+      /** feed next playable */
       [ startTimeWeb, endTimeWeb, deltaIndex ] = playQueue[0];
       this.currentTime = startTimeWeb;
+
+      if (playQueue.length <= 2) {
+        const transitionLength = endTimeWeb - this.currentTime;
+        /** main transition marker */
+        $('.main').removeClass('transition-fade-out');
+        /** transition fade in video background */
+        $(this).css({
+          'transition': `opacity ${transitionLength}s ease-in-out`,
+          '-moz-transition': `opacity ${transitionLength}s ease-in-out`,
+          '-webkit-transition': `opacity ${transitionLength}s ease-in-out`,
+          'opacity': 1
+        });
+      }
     }
+    /** stabilize currentTime */
     if (this.currentTime < startTimeWeb) {
       this.currentTime = startTimeWeb;
     }
-    this.playbackRate = playbackRate;
+    if (isSkip) {
+      const transitionLength = endTimeWeb - this.currentTime;
+      /** main transition marker */
+      $('.main').addClass('transition-fade-out');
+      /** transition fade out video background */
+      $(this).css({
+        'transition': `opacity ${transitionLength}s ease-in-out`,
+        '-moz-transition': `opacity ${transitionLength}s ease-in-out`,
+        '-webkit-transition': `opacity ${transitionLength}s ease-in-out`,
+        'opacity': 0
+      });
+      isSkip = false;
+    }
   });
 
+  let processing = false;
   $('.main').onepage_scroll({
     sectionContainer: 'section',
-    easing: 'ease',
+    easing: 'none',
     animationTime: 1000,
     pagination: true,
     updateURL: false,
-    afterMove: (index) => {
-      console.log({x: index})
-    },
     beforeMove: (index) => {
-      this.disabled = true;
-      const isForward =  currentIndex < (index - 1);
-      const currentIndexPointer = (index - 1);
-      for (
-        let i = currentIndex;
-        isForward ? i <= currentIndexPointer: i >= currentIndexPointer;
-        isForward ? i++ : i--
-      ) {
-        if (i !== currentIndex) {
-          if (isForward) {
+      if (!processing) {
+        processing = true;
+        this.disabled = true;
+        const isForward =  currentIndex < (index - 1);
+        const currentIndexPointer = (index - 1);
+        let playQueueFeed = [... playQueue];
+        isSkip = Math.abs(currentIndex - currentIndexPointer) > 1;
+        /** insert backward and forward then loop */
+        for (
+          let i = currentIndex;
+          isForward ? i <= currentIndexPointer: i >= currentIndexPointer;
+          isForward ? i++ : i--
+        ) {
+          if (i !== currentIndex && currentIndexPointer === i) {
+            if (isForward && positions[i]) {
+              playQueueFeed.push([ ... convertTimeRange(positions[i].forward), i]);
+            } else {
+              if (positions[i + 1]) {
+                playQueueFeed.push([ ... convertTimeRange(positions[i + 1].back), i + 1]);
+              }
+            }
             if (positions[i]) {
-              playQueue.push([ ... convertTimeRange(positions[i][isMobile ? 'mobile' : 'web'].forward), i]);
+              playQueueFeed.push([ ... convertTimeRange(positions[i].now), i]);
             }
-          } else {
-            if (positions[i + 1]) {
-              playQueue.push([ ... convertTimeRange(positions[i + 1][isMobile ? 'mobile' : 'web'].back), i + 1]);
-            }
-          }
-          if (positions[i]) {
-            playQueue.push([ ... convertTimeRange(positions[i][isMobile ? 'mobile' : 'web'].now), i]);
           }
         }
+        /** always take the last 3 only */
+        if (playQueueFeed.length > 3) {
+          playQueueFeed = playQueueFeed.slice(playQueueFeed.length - 3, playQueueFeed.length);
+        }
+        currentIndex = index - 1;
+        playQueue = playQueueFeed;
+        processing = false;
       }
-      currentIndex = index - 1;
-      playbackRate = 1;
     },
     loop: false,
     keyboard: true,
     responsiveFallback: false,
     direction: 'vertical'
   });
+
+  initializeVideo();
 }(window.jQuery);
